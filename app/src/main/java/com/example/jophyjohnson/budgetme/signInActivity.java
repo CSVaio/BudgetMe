c7b6136e77792ffdb61f5fef445f496ea8158570
Jophy Johnson <johnson.jophy27@gmail.com>
2016-01-24 21:38:57 -0600

    google+ sign in button added with NO click event added yet

--- app/src/main/java/com/example/jophyjohnson/budgetme/signInActivity.java
+++ app/src/main/java/com/example/jophyjohnson/budgetme/signInActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 /**
  * Created by Jophy Johnson on 1/24/2016.
  */
 public class signInActivity extends AppCompatActivity{
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,19 @@ public class signInActivity extends AppCompatActivity{
         setContentView(R.layout.login_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                //.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 }
